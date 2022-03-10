facfcafc3d0a036129d860cdb6268a1ecd4b5d6f
Nalin Aswani <nalinaswani@gmail.com>
2022-03-11 04:20:16 +1000

    Working view controller

---
+++ News-App-CBA/Controllers/ListViewController.swift
@@ -0,0 +1,70 @@
+//
+//  ViewController.swift
+//  News-App-CBA
+//
+//  Created by Nalin Aswani on 9/03/22.
+//
+
+import UIKit
+
+class ListViewController: UIViewController {
+    let tableView = UITableView()
+    
+    let parser = ParserViewModelProtocol()
+    var articles: [Article] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        title = "Nalin's News "
+        setupTableView()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        tableView.frame = view.bounds // This is to see the frame of the table
+    }
+    
+    ///Initialises the tableview
+    func setupTableView() {
+        view.addSubview(tableView)
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {  // reloads the table when the view appears
+        reloadTableViewData()
+    }
+    
+    func reloadTableViewData() {
+        articles = parser.getArticleObjects()
+    }
+}
+
+extension ListViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return articles.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        let article = articles[indexPath.row] // Gets the article by looking at the number of the row
+        let title = article.title
+        
+        cell.textLabel?.text = "\(title)" // Here we are simply naming each cell item in the table by the article name
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true) // This deselects a row after it is tapped on by a user
+        
+//        let article = articles[indexPath.row]
+//        if let url = URL(string: article.url) { // When you tap on a row, it goes to the news link
+//            UIApplication.shared.open(url)
+//        }
+    }
+}
+
