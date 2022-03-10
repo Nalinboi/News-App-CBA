4b0b411743b18e87aefe789606f9cb767fad00eb
Nalin Aswani <nalinaswani@gmail.com>
2022-03-10 15:35:22 +1000

    Creating article model

---
+++ News-App-CBA/Models/ArticleModel.swift
@@ -0,0 +1,32 @@
+//
+//  Article.swift
+//  News-App-CBA
+//
+//  Created by Nalin Aswani on 10/03/22.
+//
+
+import Foundation
+
+//https://newscatcherapi.com/free-news-api
+
+struct Root : Codable {
+    let status : String // Must have a status
+    let totalResults : Int // Must have results
+    let articles : [Article] // Must have articles
+}
+
+struct Article : Codable {
+    let source : Source // Must have source
+    let author : String?
+    let title : String // Must have title
+    let description : String?
+    let url : String // Must have url
+    let urlToImage : [Article]?
+    let publishedAt : Date // Must have a date published at
+    let content : String?
+}
+
+struct Source : Codable {
+    let id : String?
+    let name : String // Must have source name
+}
---
+++ News-App-CBA/ViewModels/ParserViewModelProtocol.swift
@@ -0,0 +1,9 @@
+//
+//  Parser.swift
+//  News-App-CBA
+//
+//  Created by Nalin Aswani on 10/03/22.
+//
+
+import Foundation
+
