9b96a340468ff99fe5d6200517dcb1a5a5117a1f
Nalin Aswani <nalinaswani@gmail.com>
2022-03-11 02:09:59 +1000

    Putting logic in viewmodel

--- News-App-CBA/ViewModels/ParserViewModelProtocol.swift
+++ News-App-CBA/ViewModels/ParserViewModelProtocol.swift
@@ -25,4 +25,24 @@ struct ParserViewModelProtocol {
         return ""  // TODO: Do error handling for api failures
         // There are two status', there is ok, and there is error. 
     }
+    
+    func getArticleObjects() -> ApiRoot {
+        let json:String = parseNewsApi() // The get request returns a string
+        // print(json)
+        let jsonData = Data(json.utf8) // we format this string into utf8, and change type to data
+        
+        // We then use the data above to decode the data into a json object below.
+        // Here I use try! This is to ensure that if the parsing fails it will crash my program.
+        // TODO: Perhaps rather than crashing the whole app due to an api, we provide a fail safe.
+        
+        let myDecoder = JSONDecoder()
+        myDecoder.dateDecodingStrategy = .iso8601 // Using a different date decoding stratagy to decode the specific type of date given from this api
+        
+        let articleObjects = try! myDecoder.decode(ApiRoot.self, from: jsonData)
+        print(type(of: articleObjects))
+        print(articleObjects.status)
+        print(articleObjects.totalResults)
+        
+        return articleObjects
+    }
 }
