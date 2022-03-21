5fe7393340b016483b4fb37ecf5babb489384b25
Nalin Aswani <nalinaswani@gmail.com>
2022-03-21 13:49:46 +1000

    Configured cards

--- News-App-CBA/Views/ListViewCell.swift
+++ News-App-CBA/Views/ListViewCell.swift
@@ -11,20 +11,21 @@ class ListViewCell: UITableViewCell {
     
     var thumbnail = UIImageView()
     var title = UILabel()
+    var sourceTitle = UILabel()
+    var datePublished = UILabel()
     
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         addSubview(thumbnail)
         addSubview(title)
+        addSubview(sourceTitle)
+        addSubview(datePublished)
         
         configureThumbnail()
         configureTitle()
-        
-        setThumbnailConstraints()
-        setTitleConstraints()
-        
-        addShadow()
+        configureSourceTitle()
+        configureDatePublished()
     }
     
     func addShadow() {
@@ -35,6 +36,9 @@ class ListViewCell: UITableViewCell {
     }
     
     func set(article: Article) {
+        datePublished.text = article.publishedAt.description
+        sourceTitle.text = article.source.name
+        
         if let urlstring = article.urlToImage { // setting the table cell to be a thumbnail image (loading) followed by the article title
             if let url = URL(string: urlstring) {
                 thumbnail.load(url: url)
@@ -53,22 +57,38 @@ class ListViewCell: UITableViewCell {
         self.thumbnail.image = nil
     }
     
+    func configureDatePublished() {
+        datePublished.font = UIFont.systemFont(ofSize: 12, weight: UIFont.Weight.ultraLight)
+        datePublished.numberOfLines = 0
+        datePublished.adjustsFontSizeToFitWidth = true
+        setDateConstraints()
+    }
+    
+    func configureSourceTitle() {
+        sourceTitle.font = UIFont.systemFont(ofSize: 18, weight: UIFont.Weight.light)
+        sourceTitle.numberOfLines = 0
+        sourceTitle.adjustsFontSizeToFitWidth = true
+        setSourceTitleConstraints()
+    }
+    
     func configureThumbnail() {
         thumbnail.layer.cornerRadius = 10
         thumbnail.clipsToBounds = true
+        setThumbnailConstraints()
     }
     
     func configureTitle() {
         title.font = UIFont.systemFont(ofSize: 20, weight: UIFont.Weight.bold)
         title.numberOfLines = 0
         title.adjustsFontSizeToFitWidth = true
-        
+        setTitleConstraints()
     }
+
     
     func setThumbnailConstraints() {
         thumbnail.translatesAutoresizingMaskIntoConstraints = false
         
-        thumbnail.centerYAnchor.constraint(equalTo: centerYAnchor, constant: -70).isActive = true
+        thumbnail.centerYAnchor.constraint(equalTo: centerYAnchor, constant: -65).isActive = true
         thumbnail.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
         thumbnail.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 12).isActive = true
 
@@ -84,13 +104,27 @@ class ListViewCell: UITableViewCell {
     func setTitleConstraints() {
         title.translatesAutoresizingMaskIntoConstraints = false
         title.topAnchor.constraint(equalTo: centerYAnchor, constant: 50).isActive = true
-        title.leadingAnchor.constraint(equalTo: thumbnail.leadingAnchor, constant: 12).isActive = true
+        title.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 12).isActive = true
         title.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -12).isActive = true
         
         //        title.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
         //        title.leadingAnchor.constraint(equalTo: thumbnail.trailingAnchor, constant: 20).isActive = true
         //        title.heightAnchor.constraint(equalToConstant: 80).isActive = true
         //        title.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -12).isActive = true
+    }
+    
+    func setSourceTitleConstraints() {
+        sourceTitle.translatesAutoresizingMaskIntoConstraints = false
+        sourceTitle.topAnchor.constraint(equalTo: bottomAnchor, constant: -30).isActive = true
+        sourceTitle.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 12).isActive = true
+        sourceTitle.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -12).isActive = true
+    }
+    
+    func setDateConstraints() {
+        datePublished.translatesAutoresizingMaskIntoConstraints = false
+        datePublished.topAnchor.constraint(equalTo: bottomAnchor, constant: -25).isActive = true
+        datePublished.leadingAnchor.constraint(equalTo: trailingAnchor, constant: -180).isActive = true
+        datePublished.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -12).isActive = true
     }
     
 }
